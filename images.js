f26905e47c9dd0d0f115af6537331c64daf8f4f3
Eugene <eugenebobalo@gmail.com>
2020-11-16 20:09:02 -0600

    finished 1rst lab

---
+++ images.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+
+    $("button").click(runLoops);
+
+
+    function runLoops() {
+        // Clear out all existing images
+        $(".imageHolder").empty();
+
+        // Get the number from the box
+        var num = parseInt($("#numPics").val());
+
+        // Print the desired number of images for each loop
+        $("#while-num").text(num + 1);
+        $("#do-while-num").text(num - 1);
+        $("#for-num").text(num);
+
+
+        /*
+         Create a while loop that runs "num + 1" times.
+
+         The body of the loop should contain the statement:
+         addImageToDiv("#while-loop");
+        */
+        var count = 0;
+        while (count != num + 1) {
+            addImageToDiv("#while-loop");
+            count++;
+        }
+
+
+
+
+        /*
+         Create a do-while loop that runs "num - 1" times.
+
+         The body of the loop should contain the statement:
+         addImageToDiv("#do-while-loop");
+        */
+        var count = 0;
+        do {
+            addImageToDiv("#do-while-loop");
+            count++;
+        } while (count != num - 1)
+
+
+
+        /*
+         Create a for loop that runs exactly "num" times.
+
+         The body of the loop should contain the statement:
+         addImageToDiv("#for-loop");
+        */
+        for (var i = 0; i < num; i++) {
+            addImageToDiv("#for-loop");
+        }
+
+
+
+
+    }
+
+    function addImageToDiv(divId) {
+        var fileName = $(":radio[name='pictureSelect']:checked").val();
+        // Create a new image element
+        var imageElement = $("<img>");
+        // Set its source attribute
+        imageElement.attr("src", `img/${fileName}`);
+        // Add it as a child of another element
+        $(divId).append(imageElement);
+    }
+
+});
